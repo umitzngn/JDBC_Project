8ab1605724488c5bef5c895e49cf82b8f779a1d9
umitzngn <60893281+umitzngn@users.noreply.github.com>
2020-10-07 18:27:05 +0200

    new added

--- src/test/java/add_file_here/EU3_Practice.java
+++ src/test/java/add_file_here/EU3_Practice.java
@@ -2,6 +2,6 @@ package add_file_here;
 
 public class EU3_Practice {
     public static void main(String[] args) {
-        System.out.println("EU3 is working as usual");
+        System.out.println("EU3 is working as usual ");
     }
 }
